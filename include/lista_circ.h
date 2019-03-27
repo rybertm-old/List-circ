e7dde29a0b29f288de53cac7f6af99b7e2035987
Robert Morais (rybertm) <bitmoraes@hotmail.com>
2019-03-27 08:18:47 -0300

    Header Files

---
+++ include/lista_circ.h
@@ -0,0 +1,32 @@
+﻿#ifndef LISTA_CIRC_H
+#define LISTA_CIRC_H
+
+#include <iostream>
+
+template <typename T>
+class lista_circ
+{
+private:
+	struct noh
+	{
+		T m_dado;
+		noh *m_prox;
+	};
+
+	noh *m_head;
+
+public:
+	lista_circ();
+	~lista_circ();
+
+	void insere(const T &dado);
+	void remove(const T &dado);
+	void remove(const noh *dado);
+	void imprime();
+	noh *buscar(const T &valor);
+	void esvazia();
+	int comprimento();
+	void troca(const T &original, const T &novo);
+};
+
+#endif // LISTA_CIRC_H
